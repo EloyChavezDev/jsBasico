b4a0294d4b72683654d3fbb56062a883d719ac6b
J. Eloy Chavez <laneloychavez@gmail.com>
2021-07-28 09:22:35 -0500

    V 1.0
    Repositorio con Conceptos Básicos y ejemplos de Javascript

---
+++ 1-valores.js
@@ -0,0 +1,24 @@
+//Tipos Primitivos
+
+// Numeros
+40
+3.14
+
+//Strings (Cadenas de Texto) Esto es la forma en la que puedo crear algo que se pueda imprimir en la pantalla, o escribir información, que el usuario pueda entender y leer. utilizar palabras, o frases.
+"Diego de Granda"
+
+//Boleanos estos fueron inventados por George Boole, un Matemático británico. y esto hace referencia a cosas que podamos validar.
+true
+false
+
+// Valores vacios (Empty values), es como un placeholder (espacio reservado para algun valor) de algún valor faltante.
+null
+undefined
+
+// Tipos de objetos o no primitivos
+
+// corchetes // arrays
+[1,2,3]
+
+// llaves // objetos
+{ nombre: "Diego" }
---
+++ 3-funciones.js
@@ -0,0 +1,16 @@
+// Las funciones son procedimientos, un conjunto de sentencias o pasos que realizarán una tarea o cálculo con ciertos valores.
+
+// Declarativas
+function miFuncion(){
+    return 3;
+}
+
+// mandamos llamar la funcion
+miFuncion();
+
+//Expresion (también conocidas como funciones anónimas)
+var miFuncion = function(a,b){
+    return a + b;
+}
+
+miFuncion();
